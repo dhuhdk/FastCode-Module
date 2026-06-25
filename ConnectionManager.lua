56c98db849503df4f4fb3f05858ccb4e293d82a5
dhuhdk <accphucuaminhhuy@gmail.com>
2026-06-26 00:51:04 +0700

    hey

---
+++ ConnectionManager.lua
@@ -0,0 +1,47 @@
+local ConnectionManager = {}
+ConnectionManager.__index = ConnectionManager
+
+function ConnectionManager.new(Name)
+    local self = setmetatable({}, ConnectionManager)
+    self.Name = Name
+    self.Events = {}
+    return self
+end
+
+function ConnectionManager:Bind(Key, Connection)
+    assert(type(Key) == "string", "Arg1 must be a string.")
+    assert(typeof(Connection) == "RBXScriptConnection", "Arg2 must be an event (RBXScriptConnection).")
+    if self.Events[Key] and self.Events[Key].Connected then self.Events[Key]:Disconnect() end
+    self.Events[Key] = Connection
+    return Connection
+end
+
+function ConnectionManager:Remove(Key, Bool)
+    assert(type(Key) == "string", "Arg1 must be a string value.")
+    Bool = (Bool == nil and false) or Bool
+    assert(type(Bool) == "boolean", "Arg2 must be a boolean value.")
+    local OldConnection = self.Events[Key]
+    if OldConnection then
+        if Bool and OldConnection.Connected then OldConnection:Disconnect() end
+        self.Events[Key] = nil
+    end
+    return OldConnection
+end
+
+function ConnectionManager:Disconnect(Key)
+    assert(type(Key) == "string", "Arg must be a string value.")
+    local OldConnection = self.Events[Key]
+    if OldConnection then if OldConnection.Connected then OldConnection:Disconnect() end end
+    return OldConnection
+end
+
+function ConnectionManager:Clear()
+    Bool = (Bool == nil and false) or Bool
+    assert(type(Bool) == "boolean", "Arg2 must be a boolean value.")
+    for K, Conn in pairs(self.Events) do
+        if Conn.Connected then Conn:Disconnect() end
+    end
+    table.clear(self.Events)
+end
+
+return ConnectionManager
