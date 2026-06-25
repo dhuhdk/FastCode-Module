ced44b9eea5637a9009b4296a7205fda455a6494
dhuhdk <accphucuaminhhuy@gmail.com>
2026-06-25 23:53:35 +0700

    1

--- Module.lua
+++ Module.lua
@@ -26,6 +26,7 @@ function FastCode:GetPlayerContext(PlayerName)
         Plr = IsPlayerAvailable(PlayerName)
     else
         Plr = Plrs.LocalPlayer
+        Backpack = Plr:WaitForChild("Backpack")
         PlrGui = Plr:WaitForChild("PlayerGui")
         PlrScript = Plr:WaitForChild("PlayerScripts")
     end
@@ -33,18 +34,40 @@ function FastCode:GetPlayerContext(PlayerName)
     Char = Plr.Character or Plr.CharacterAdded:Wait()
     Hum = Char:WaitForChild("Humanoid")
     Root = Char:WaitForChild("HumanoidRootPart")
-    Backpack = Plr:WaitForChild("Backpack")
     Animator = Hum:WaitForChild("Animator")
     self.Plr = Plr; self.Char = Char; self.Hum = Hum; self.Root = Root; self.Backpack = Backpack; self.Animator = Animator
     Plr.CharacterAdded:Connect(function(Char)
         local NChar = Char
         local NHum = NChar:WaitForChild("Humanoid")
         local NRoot = NChar:WaitForChild("HumanoidRootPart")
-        local NBackpack = NPlr:WaitForChild("Backpack")
         local NAnimator = NHum:WaitForChild("Animator")
-        self.Char = NChar; self.Hum = NHum; self.Root = NRoot; self.Backpack = NBackpack; self.Animator = NAnimator
+        self.Char = NChar; self.Hum = NHum; self.Root = NRoot; self.Animator = NAnimator
     end)
     return self
+end
+
+function FastCode:GetService()
+    local RS = game:GetService("RunService")
+    local UIS = game:GetService("UserInputService")
+    local VIM = game:GetService("VirtualInputManager")
+    local TS = game:GetService("TweenService")
+    local TCS = game:GetService("TextChatService")
+    local CAS = game:GetService("ContextActionService")
+    self.RS = RS; self.UIS = UIS; self.VIM = VIM; self.TS = TS; self.TCS = TCS; self.CAS = CAS
+    return self
+end
+
+function FastCode:InfiniteYield()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end
+
+function FastCode.Track(Animator, AnimationId)
+    if typeof(Animator) ~= "Instance" or not Animator:IsA("Animator") then error("Arg1 need to be an animator instance.") end
+    if type(AnimationId) ~= "string" then error("AnimationId must be a string value.") end
+    local Animation = Instance.new("Animation")
+    Animation.AnimationId = AnimaionId
+    local Track = Animator:LoadAnimation(Animation)
+    return Track, Animation
 end
 
 return FastCode
