0e44950cae01618a5798b5b5d6bfd211ef6ab0c8
dhuhdk <accphucuaminhhuy@gmail.com>
2026-06-26 14:35:13 +0700

    Update FastCode.lua

--- FastCode.lua
+++ FastCode.lua
@@ -1,4 +1,4 @@
-local ConnectionManager = loadstring(game:HttpGet:("https://raw.githubusercontent.com/dhuhdk/FastCode-Module/refs/heads/main/ConnectionManager.lua?token=GHSAT0AAAAAAEA5YTEPDE7W5EPZE5UQJ34S2R5NSZQ"))()
+local ConnectionManager = loadstring(game:HttpGet:("https://cdn.jsdelivr.net/gh/dhuhdk/FastCode-Module@main/ConnectionManager.lua"))()
 
 local FastCode = {}
 FastCode.ConnectionManager = {}
